d9d0120f8aec7cb4a54c648bd369b060447ef82f
Maverick <brazillmav@gmail.com>
2023-07-18 08:43:51 -0400

    Initial Commit

---
+++ WeSplit (100 Days of SwiftUI)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeSplit (100 Days of SwiftUI)
+//
+//  Created by Maverick Brazill on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit (100 Days of SwiftUI)/WeSplit__100_Days_of_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit__100_Days_of_SwiftUI_App.swift
+//  WeSplit (100 Days of SwiftUI)
+//
+//  Created by Maverick Brazill on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit__100_Days_of_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
